6f8868dd13e1e9c196e4fd1909d3c5265db335f1
gameok <sos1357810@gmail.com>
2017-12-10 22:02:44 +0800

    Update 1_Two_Sum.cpp

--- Desktop/1_Two_Sum.cpp
+++ Desktop/1_Two_Sum.cpp
@@ -1,17 +1,25 @@
+/*
+方法：哈希表
+耗时：6ms
+*/
+
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map <int, int> indices;
-        for (int i = 0; i < nums.size(); i++) {
+        unordered_map <int, int> indices;   //初始化哈希表
+        for (int i = 0; i < nums.size(); i++) {     //遍历数组，将数组索引存储到哈希表中
             indices[nums[i]] = i;
         }
         
-        for (int i = 0; i < indices.size(); i++) {
-            int left = target - nums[i];
-            if (indices.count(left) && indices[left] != i) {
+        for (int i = 0; i < indices.size(); i++) {  //遍历哈希表
+            int left = target - nums[i];    //若找到nums[i]，则找一个值left，让left和nums[i]的和为target
+            if (indices.count(left) && indices[left] != i) {    
+                                    //indices.count(left)表示存在这样一个left值，indices[left] != i表示数组中的元素不可重复使用
                 return {i, indices[left]};
+                                    //若找到满足条件的两个元素，就返回他们的下标
             }
         }
-        return {};
+        return {};  //若找不到满足条件的两个元素，就返回一个空的vector
     }
 };
